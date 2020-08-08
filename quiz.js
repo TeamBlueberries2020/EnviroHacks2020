bc256d39ab3da78efe2ddb82f81f8609b0b68344
TeamBlueberries2020 <69370395+TeamBlueberries2020@users.noreply.github.com>
2020-08-07 20:58:06 -0500

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,18 @@
+var questionAnswers = ["","","","",""]; /*DEFAULT VALUES*/
+
+function storeVar(value, questionNumber){
+  var amount = value;
+  var qnumber = questionNumber;
+  questionAnswers[questionNumber] = value;
+  console.log(amount);
+  console.log(qnumber);
+}
+
+
+
+  //   function myFunction(val) {
+  //   var v = val.value;
+    
+  //   location.replace("quiz.html");
+    
+  // }
